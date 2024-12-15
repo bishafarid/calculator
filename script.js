e64cc531af24863fa713a3f4f63afc59bca1f618
bisha farid <bishasiddiqui228@gmail.com>
2024-12-15 11:58:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function add() {
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    document.getElementById('result').innerHTML= Number(num1)+Number(num2)
+  }
+function substract() {
+      const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    document.getElementById('result').innerHTML= Number(num1)-Number(num2)
+  }
+function multiply() {
+      const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    document.getElementById('result').innerHTML= Number(num1)*Number(num2)
+  }
+function divide() {
+      const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    document.getElementById('result').innerHTML= Number(num1)/Number(num2)
+  }
